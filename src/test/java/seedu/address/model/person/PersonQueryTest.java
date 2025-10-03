a4b810d091dd9738927c9c9ce8c38d564540ed82
Thitiwat Kosolpattanadurong <thitiwatipst@icloud.com>
2025-10-03 18:25:39 +0800

    Add test covering filter method

--- src/test/java/seedu/address/model/person/PersonQueryTest.java
+++ src/test/java/seedu/address/model/person/PersonQueryTest.java
@@ -3,11 +3,104 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.AMY;
+import static seedu.address.testutil.TypicalPersons.BENSON;
 import static seedu.address.testutil.TypicalPersons.BOB;
 
 import org.junit.jupiter.api.Test;
 
 public class PersonQueryTest {
+
+    @Test
+    public void filter_empty_expectTrue() {
+        assertTrue(PersonQuery.build().filter(ALICE));
+    }
+
+    @Test
+    public void filter_byName() {
+        PersonQuery query = PersonQuery
+                .build()
+                .setName(new String[]{"Pauline", "Doe"});
+        assertTrue(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
+
+    @Test
+    public void filter_byPhone() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setPhone(ALICE.getPhone());
+        assertTrue(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
+
+    @Test
+    public void filter_byAddress() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setAddress(ALICE.getAddress());
+        assertTrue(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
+
+    @Test
+    public void filter_byEmail() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setEmail(ALICE.getEmail());
+        assertTrue(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
+
+    @Test
+    public void filter_bySingleTag() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setTags(ALICE.getTags());
+        // ALICE tag (friends) is a subset of BENSON tag (owesMoney, friends)
+        assertTrue(query.filter(ALICE));
+        assertTrue(query.filter(BENSON));
+    }
+
+    @Test
+    public void filter_byMultipleTags() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setTags(BENSON.getTags());
+        // ALICE tag (friends) is a subset of BENSON tag (owesMoney, friends)
+        assertFalse(query.filter(ALICE));
+        assertTrue(query.filter(BENSON));
+    }
+
+    @Test
+    public void filter_byRank() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setRank(BOB.getRank());
+        assertTrue(query.filter(ALICE));
+        assertTrue(query.filter(BOB));
+        assertFalse(query.filter(AMY));
+    }
+
+    @Test
+    public void filter_chain_expectTrue() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setRank(ALICE.getRank())
+            .setPhone(ALICE.getPhone());
+        assertTrue(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
+
+    @Test
+    public void filter_chain_expectFalse() {
+        PersonQuery query = PersonQuery
+            .build()
+            .setName(new Name[]{ALICE.getName()})
+            .setPhone(BOB.getPhone());
+        assertFalse(query.filter(ALICE));
+        assertFalse(query.filter(BOB));
+    }
 
     @Test
     public void equals() {
