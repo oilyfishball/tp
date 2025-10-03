af5fb8b8a975b8ce7216616291822d1725aee219
wongyuzhen <wongyuzhenn@gmail.com>
2025-10-03 20:31:16 +0800

    Create Link Appointment Command and Parser
    1. Update AddressBookParser with link command case
    2. Update CliSyntax with appointment prefixes
    3. Implement LinkAppointmentCommand
    4. Implement LinkAppointmentCommandParser

---
+++ src/main/java/seedu/address/logic/commands/LinkAppointmentCommand.java
@@ -0,0 +1,76 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.appointment.Appointment;
+
+/**
+ * Creates an appointment and links it directly to a client (person).
+ */
+public class LinkAppointmentCommand extends Command {
+
+    public static final String COMMAND_WORD = "link";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Links a new appointment to a client. "
+            + "Parameters: "
+            + "/n CLIENT_NAME "
+            + "/appt DATE [TIME] "
+            + "[/len MINUTES] "
+            + "[/loc LOCATION] "
+            + "[/type TYPE] "
+            + "[/note NOTES] "
+            + "[/status planned|confirmed|completed|cancelled]\n"
+            + "Example: " + COMMAND_WORD + " "
+            + "/n Alex Wu "
+            + "/appt 12-10-2025 1430 "
+            + "/len 90 "
+            + "/loc Bukit Merah FSC "
+            + "/type home-visit "
+            + "/note Bring consent form "
+            + "/status planned";
+
+    public static final String MESSAGE_SUCCESS = "New appointment linked: %1$s";
+
+    private final String clientName;
+    private final Appointment appointment;
+
+    public LinkAppointmentCommand(String clientName, Appointment appointment) {
+        requireNonNull(clientName);
+        requireNonNull(appointment);
+        this.clientName = clientName;
+        this.appointment = appointment;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+
+        // For now just return a success message (link logic to be implemented in Model later).
+        return new CommandResult(String.format(MESSAGE_SUCCESS,
+                clientName + " at " + appointment.toString()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if (!(other instanceof LinkAppointmentCommand)) {
+            return false;
+        }
+        LinkAppointmentCommand otherCmd = (LinkAppointmentCommand) other;
+        return clientName.equalsIgnoreCase(otherCmd.clientName)
+                && appointment.equals(otherCmd.appointment);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .add("clientName", clientName)
+                .add("appointment", appointment)
+                .toString();
+    }
+}
---
+++ src/main/java/seedu/address/logic/parser/LinkAppointmentCommandParser.java
@@ -0,0 +1,69 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LENGTH;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LOCATION;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TYPE;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.LinkAppointmentCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.appointment.Appointment;
+import seedu.address.model.appointment.AppointmentDateTime;
+import seedu.address.model.appointment.AppointmentLength;
+import seedu.address.model.appointment.AppointmentLocation;
+import seedu.address.model.appointment.AppointmentNote;
+import seedu.address.model.appointment.AppointmentStatus;
+import seedu.address.model.appointment.AppointmentType;
+
+/**
+ * Parses input arguments and creates a new LinkAppointmentCommand object.
+ */
+public class LinkAppointmentCommandParser implements Parser<LinkAppointmentCommand> {
+
+    @Override
+    public LinkAppointmentCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args,
+                        PREFIX_NAME, PREFIX_APPOINTMENT, PREFIX_LENGTH,
+                        PREFIX_LOCATION, PREFIX_TYPE, PREFIX_NOTE, PREFIX_STATUS);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_APPOINTMENT)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    LinkAppointmentCommand.MESSAGE_USAGE));
+        }
+
+        argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_APPOINTMENT,
+                PREFIX_LENGTH, PREFIX_LOCATION, PREFIX_TYPE, PREFIX_NOTE, PREFIX_STATUS);
+
+        String clientName = argMultimap.getValue(PREFIX_NAME).get();
+
+        AppointmentDateTime dateTime =
+                ParserUtil.parseAppointmentDateTime(argMultimap.getValue(PREFIX_APPOINTMENT).get());
+
+        AppointmentLength length = ParserUtil.parseAppointmentLength(
+                argMultimap.getValue(PREFIX_LENGTH).orElse(AppointmentLength.DEFAULT));
+        AppointmentLocation location = ParserUtil.parseAppointmentLocation(
+                argMultimap.getValue(PREFIX_LOCATION).orElse(AppointmentLocation.NO_LOCATION));
+        AppointmentType type = ParserUtil.parseAppointmentType(
+                argMultimap.getValue(PREFIX_TYPE).orElse(AppointmentType.NO_TYPE));
+        AppointmentNote note = ParserUtil.parseAppointmentNote(
+                argMultimap.getValue(PREFIX_NOTE).orElse(AppointmentNote.NO_NOTE));
+        AppointmentStatus status = ParserUtil.parseAppointmentStatus(
+                argMultimap.getValue(PREFIX_STATUS).orElse(AppointmentStatus.PLANNED));
+
+        Appointment appointment = new Appointment(dateTime, length, location, type, note, status);
+
+        return new LinkAppointmentCommand(clientName, appointment);
+    }
+
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+}
