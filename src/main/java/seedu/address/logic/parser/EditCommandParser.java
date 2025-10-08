29f1b2e601eaa7d4f18ae647af205038ccb057b0
Zhao Rui <zhaorui031202@gmail.com>
2025-10-09 01:16:29 +0800

    Minor format updates

--- src/main/java/seedu/address/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditCommandParser.java
@@ -2,6 +2,7 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_APPOINTMENT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
@@ -32,15 +33,26 @@ public class EditCommandParser implements Parser<EditCommand> {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                        PREFIX_ADDRESS, PREFIX_TAG, PREFIX_RANK);
+                        PREFIX_ADDRESS, PREFIX_TAG, PREFIX_RANK, PREFIX_APPOINTMENT);
 
         String name = args.trim().split(" /", 2)[0];
 
         argMultimap.verifyNoDuplicatePrefixesFor(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL,
-                PREFIX_ADDRESS, PREFIX_RANK);
+                PREFIX_ADDRESS, PREFIX_RANK, PREFIX_APPOINTMENT);
 
         EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+        setEditPersonDescriptor(editPersonDescriptor, argMultimap);
+        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
 
+        if (!editPersonDescriptor.isAnyFieldEdited()) {
+            throw new ParseException(EditCommand.MESSAGE_INVALID_SYNTAX);
+        }
+
+        return new EditCommand(name, editPersonDescriptor);
+    }
+
+    private void setEditPersonDescriptor(EditPersonDescriptor editPersonDescriptor, ArgumentMultimap argMultimap)
+            throws ParseException {
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
             editPersonDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
         }
@@ -53,17 +65,9 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (argMultimap.getValue(PREFIX_ADDRESS).isPresent()) {
             editPersonDescriptor.setAddress(ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get()));
         }
-        parseTagsForEdit(argMultimap.getAllValues(PREFIX_TAG)).ifPresent(editPersonDescriptor::setTags);
-
         if (argMultimap.getValue(PREFIX_RANK).isPresent()) {
             editPersonDescriptor.setRank(ParserUtil.parseRank(argMultimap.getValue(PREFIX_RANK).get()));
         }
-
-        if (!editPersonDescriptor.isAnyFieldEdited()) {
-            throw new ParseException(EditCommand.MESSAGE_INVALID_SYNTAX);
-        }
-
-        return new EditCommand(name, editPersonDescriptor);
     }
 
     /**
